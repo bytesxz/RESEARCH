99a016a4e3cbdee8db41a1671da23c96b42c646f
bytesxz <jairusian1234567@gmail.com>
2020-02-15 11:11:55 +0800

    Add files via upload
    allow dust readings to control vac motor

--- sb/app.ino
+++ sb/app.ino
@@ -1,7 +1,7 @@
 /* RESEARCHHHHHHH
 Button:
-  - pushbutton attached to pin 22 from +3V (5v already used for DSM)
-  - 10K resistor attached to pin 22 from ground
+  - pushbutton attached to pin 2 from +3V (5v already used for DSM)
+  - 10K resistor attached to pin 2 from ground
 Motor:
   - vacuum motor connected to d12
   - roller/eraser to d13
@@ -9,8 +9,8 @@ Sensor:
   -
 */
 //DSM
-int parM1 = 8;
-int parM2 = 9;
+int parM1 = 8; // for 1um particles
+int parM2 = 9; // for 2.5um particles
 unsigned long duration;
 unsigned long starttime;
 unsigned long sampletime_ms = 5000; // rate @ which the values will be measured and given.
@@ -23,8 +23,8 @@ unsigned long concenOT25 = 0;
 unsigned long currentConcentration = 0;
 //Motor
 int vacMotor = 12; // controlled by pwm output, based on DSM readings
-int eRoller = 13; // just 1
-int outputvalue
+int eRoller = 3; // just 1
+int outputvalue;
 //switch
 int bPin = 2;
 int buttonstate = 0;
@@ -33,7 +33,7 @@ int buttonstate = 0;
 unsigned long sizeOne() { // unsigned long functions... what the fuck
   duration = pulseIn(parM1, LOW);
   LPulseOc = LPulseOc + duration;
-  if ((millis()-starttime) >= sampletime_ms) //if the sample time is less than 5s
+  if ((millis()-starttime) >= sampletime_ms) //if the sample time < statedT
   {
     ratio = LPulseOc/(sampletime_ms*10.0);  
     Serial.print("Concentration = ");
@@ -74,7 +74,7 @@ void wait(int timew) {
     }
 }
 
-unsigned long getaverage(int size, int limit) {
+unsigned long getaverage(int size, int limit) {// note that size can only be 1 or 2, since other values will just be ignored by this function
   unsigned long baselinevalue [limit];
   unsigned long averagevalue = 0;
   if (size == 1) {
@@ -89,6 +89,7 @@ unsigned long getaverage(int size, int limit) {
   }
   for (int j = 0; j <= limit; j++) {
     averagevalue = averagevalue + baselinevalue [j];
+    Serial.println("getting baseline values. Please wait");
   }
   averagevalue = averagevalue / limit;
   return averagevalue;
@@ -107,11 +108,32 @@ int trackbutton(int BUTTON_PIN) {// will loop until it detects a button press, i
   return run;
 }
 
-void start() {
+void startmotor(int motor_pin, int state) {
+  if (state == 0) {
+      digitalWrite(motor_pin, LOW);
+  }
+  else if (state == 1) {
+    digitalWrite(motor_pin, HIGH);
+  }
+  else {
+    Serial.println("what the fuck yo");// editk for finalization
+  }
+}
+
+void motorevent(int motor_pin) {
+  unsigned long recent = sizeTwoFive();
+  if (recent >= 500) {
+    startmotor(motor_pin, 1);
+  }
+  else {
+    startmotor(motor_pin, 0);
+  }
+}
+
+void rMotor() {
  int state = trackbutton(bPin);
  if (state == 1) {
    Serial.println("1");
-   digitalWrite(eRoller, HIGH); // turns on roller motor
    
  }
  else {
@@ -126,13 +148,16 @@ void setup() {
   Serial.begin(9600);
   pinMode(parM1, INPUT);
   pinMode(parM2, INPUT);
+  pinMode(eRoller, OUTPUT);
+  pinMode(vacMotor, OUTPUT);
   starttime = millis(); //start to read time in ms
   pinMode(bPin, INPUT); // state button pin as an input
+  
 }
 
 void loop() {
- // concenOT25 = getaverage(2.5, 10); // gets baseline dust amount.
-  start();
-
-
+  motorevent(vacMotor);
+  delay(5000);
+  motorevent(eRoller);
+  delay(5000);
 }
